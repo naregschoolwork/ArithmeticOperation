1ad00466196ede07af701a5444c294fbb450c1b2
Nareg Mouradian <67912337+naregschoolwork@users.noreply.github.com>
2022-03-24 19:50:34 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -19,15 +19,15 @@ public class ArithmeticOperationsTest {
 		//fail("must fail");
 
 	}
-    @Test
-    public void testSub()
-    {
+//     @Test
+//     public void testSub()
+//     {
 
-        ArithmeticOperations operations = new ArithmeticOperations();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
-        assertEquals(expected, actual);
+//         ArithmeticOperations operations = new ArithmeticOperations();
+//         Integer actual = operations.addOrSub(6, 2);
+//         Integer expected = 4;
+//         assertEquals(expected, actual);
 
-    }
+//     }
 
 }
